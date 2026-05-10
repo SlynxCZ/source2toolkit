10a08a216d276f2dfe9a18a58c34f3de05ad2847
Michal Přikryl <slynx@funplay.pro>
2026-05-10 19:29:23 +0200

    Update build sequence

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,32 +10,32 @@ fi
 export GITHUB_SHA_SHORT="$(git rev-parse --short HEAD)"
 
 ### --- Download HL2SDK-CS2 + Metamod-Source -------------------------------
-SDK_DIR="/tmp/sdk"
-HL2SDK_DIR="$SDK_DIR/hl2sdk-cs2"
-MMSOURCE_DIR="$SDK_DIR/metamod-source"
-CSGO_PROTO_DIR="$SDK_DIR/Protobufs"
+# SDK_DIR="/tmp/sdk"
+# HL2SDK_DIR="$SDK_DIR/hl2sdk-cs2"
+# MMSOURCE_DIR="$SDK_DIR/metamod-source"
+# CSGO_PROTO_DIR="$SDK_DIR/Protobufs"
 
-echo "=== Preparing temporary SDK directory ==="
-rm -rf "$SDK_DIR"
-mkdir -p "$SDK_DIR"
+# echo "=== Preparing temporary SDK directory ==="
+# rm -rf "$SDK_DIR"
+# mkdir -p "$SDK_DIR"
 
-echo "=== Downloading HL2SDK-CS2 ==="
-git clone --recursive --branch cs2 --single-branch https://github.com/alliedmodders/hl2sdk.git "$HL2SDK_DIR"
+# echo "=== Downloading HL2SDK-CS2 ==="
+# git clone --recursive --branch cs2 --single-branch https://github.com/alliedmodders/hl2sdk.git "$HL2SDK_DIR"
 
 # echo "=== Downloading Metamod-Source ==="
 # git clone --recursive --branch master --single-branch https://github.com/alliedmodders/metamod-source.git "$MMSOURCE_DIR"
 
-echo "=== Downloading Protobufs ==="
-git clone --recursive https://github.com/SteamDatabase/Protobufs "$CSGO_PROTO_DIR"
+# echo "=== Downloading Protobufs ==="
+# git clone --recursive https://github.com/SteamDatabase/Protobufs "$CSGO_PROTO_DIR"
 
-### --- Export env vars for CMake ------------------------------------------
-export HL2SDKCS2="$HL2SDK_DIR"
-export MMSOURCE_DEV="$MMSOURCE_DIR"
-export CSGO_PROTO="$CSGO_PROTO_DIR/csgo"
+# ### --- Export env vars for CMake ------------------------------------------
+# export HL2SDKCS2="$HL2SDK_DIR"
+# export MMSOURCE_DEV="$MMSOURCE_DIR"
+# export CSGO_PROTO="$CSGO_PROTO_DIR/csgo"
 
-echo "Using HL2SDKCS2=$HL2SDKCS2"
-echo "Using MMSOURCE_DEV=$MMSOURCE_DEV"
-echo "Using CSGO_PROTO=$CSGO_PROTO"
+# echo "Using HL2SDKCS2=$HL2SDKCS2"
+# echo "Using MMSOURCE_DEV=$MMSOURCE_DEV"
+# echo "Using CSGO_PROTO=$CSGO_PROTO"
 
 ### --- Build ---------------------------------------------------------------
 echo "=== Starting build ==="
@@ -44,10 +44,11 @@ rm -rf build
 mkdir build
 cd build
 
-cmake .. \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_CXX_COMPILER=g++
+cmake .. -G Ninja \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_C_COMPILER=clang-18 \
+  -DCMAKE_CXX_COMPILER=clang++-18
 
-echo "=== Building with GCC | Release | All ==="
-cmake --build . --config Release -j"$(nproc)"
+echo "=== Building with Clang | RelWithDebInfo ==="
+
+cmake --build . -j"$(nproc)"
