78fb1591941cd861c3a9d7906ae7cbdb88a407ff
Michal Přikryl <slynx@funplay.pro>
2026-05-12 17:40:34 +0200

    Update sdk to use env path

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,32 +10,37 @@ fi
 export GITHUB_SHA_SHORT="$(git rev-parse --short HEAD)"
 
 ### --- Download HL2SDK-CS2 + Metamod-Source -------------------------------
-# SDK_DIR="/tmp/sdk"
-# HL2SDK_DIR="$SDK_DIR/hl2sdk-cs2"
-# MMSOURCE_DIR="$SDK_DIR/metamod-source"
-# CSGO_PROTO_DIR="$SDK_DIR/Protobufs"
+SDK_DIR="/tmp/sdk"
+SOURCE2TOOLKITSDK_DIR="$SDK_DIR/source2toolkit-sdk"
+HL2SDK_DIR="$SDK_DIR/hl2sdk-cs2"
+MMSOURCE_DIR="$SDK_DIR/metamod-source"
+CSGO_PROTO_DIR="$SDK_DIR/Protobufs"
 
-# echo "=== Preparing temporary SDK directory ==="
-# rm -rf "$SDK_DIR"
-# mkdir -p "$SDK_DIR"
+echo "=== Preparing temporary SDK directory ==="
+rm -rf "$SDK_DIR"
+mkdir -p "$SDK_DIR"
 
-# echo "=== Downloading HL2SDK-CS2 ==="
-# git clone --recursive --branch cs2 --single-branch https://github.com/alliedmodders/hl2sdk.git "$HL2SDK_DIR"
+echo "=== Downloading Source2Toolkit-SDK ==="
+git clone --recursive https://github.com/SlynxCZ/source2toolkit-sdk.git "$SOURCE2TOOLKITSDK_DIR"
 
-# echo "=== Downloading Metamod-Source ==="
-# git clone --recursive --branch master --single-branch https://github.com/alliedmodders/metamod-source.git "$MMSOURCE_DIR"
+echo "=== Downloading HL2SDK-CS2 ==="
+git clone --recursive --branch cs2 --single-branch https://github.com/alliedmodders/hl2sdk.git "$HL2SDK_DIR"
 
-# echo "=== Downloading Protobufs ==="
-# git clone --recursive https://github.com/SteamDatabase/Protobufs "$CSGO_PROTO_DIR"
+echo "=== Downloading Metamod-Source ==="
+git clone --recursive --branch master --single-branch https://github.com/alliedmodders/metamod-source.git "$MMSOURCE_DIR"
 
-# ### --- Export env vars for CMake ------------------------------------------
-# export HL2SDKCS2="$HL2SDK_DIR"
-# export MMSOURCE_DEV="$MMSOURCE_DIR"
-# export CSGO_PROTO="$CSGO_PROTO_DIR/csgo"
+echo "=== Downloading Protobufs ==="
+git clone --recursive https://github.com/SteamDatabase/Protobufs "$CSGO_PROTO_DIR"
 
-# echo "Using HL2SDKCS2=$HL2SDKCS2"
-# echo "Using MMSOURCE_DEV=$MMSOURCE_DEV"
-# echo "Using CSGO_PROTO=$CSGO_PROTO"
+### --- Export env vars for CMake ------------------------------------------
+export SOURCE2TOOLKIT_SDK="$SOURCE2TOOLKITSDK_DIR"
+export HL2SDKCS2="$HL2SDK_DIR"
+export MMSOURCE_DEV="$MMSOURCE_DIR"
+export CSGO_PROTO="$CSGO_PROTO_DIR/csgo"
+
+echo "Using HL2SDKCS2=$HL2SDKCS2"
+echo "Using MMSOURCE_DEV=$MMSOURCE_DEV"
+echo "Using CSGO_PROTO=$CSGO_PROTO"
 
 ### --- Build ---------------------------------------------------------------
 echo "=== Starting build ==="
