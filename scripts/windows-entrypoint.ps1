8f1ec2919c9a887f0bb64cd1025cb9ddd09dd034
Michal Přikryl <slynx@funplay.pro>
2026-09-01 22:48:58 +0200

    fix: fix failing CI

--- scripts/windows-entrypoint.ps1
+++ scripts/windows-entrypoint.ps1
@@ -45,7 +45,7 @@ $SDK_DIR = "$env:TEMP\sdk"
 $SOURCE2TOOLKITSDK_DIR = "$SDK_DIR\source2toolkit-sdk"
 $HL2SDK_DIR = "$SDK_DIR\hl2sdk-cs2"
 $MMSOURCE_DIR = "$SDK_DIR\metamod-source"
-$CSGO_PROTO_DIR = "$SDK_DIR\Protobufs\csgo"
+$CSGO_PROTO_DIR = "$SDK_DIR\Protobufs"
 
 Write-Host "=== Preparing temporary SDK directory ==="
 if (Test-Path $SDK_DIR) { Remove-Item -Recurse -Force $SDK_DIR }
@@ -58,7 +58,7 @@ Write-Host "=== Downloading HL2SDK-CS2 ==="
 git clone --recursive --branch cs2 --single-branch https://github.com/alliedmodders/hl2sdk.git $HL2SDK_DIR
 
 Write-Host "=== Downloading Metamod-Source ==="
-git clone --recursive --branch k/sourcehook_alternative --single-branch https://github.com/alliedmodders/metamod-source.git $MMSOURCE_DIR
+git clone --recursive --branch master --single-branch https://github.com/alliedmodders/metamod-source.git $MMSOURCE_DIR
 
 Write-Host "=== Downloading Protobufs ==="
 git clone --recursive https://github.com/SteamDatabase/Protobufs $CSGO_PROTO_DIR
@@ -67,7 +67,7 @@ git clone --recursive https://github.com/SteamDatabase/Protobufs $CSGO_PROTO_DIR
 $env:SOURCE2TOOLKIT_SDK = $SOURCE2TOOLKITSDK_DIR
 $env:HL2SDKCS2 = $HL2SDK_DIR
 $env:MMSOURCE_DEV = $MMSOURCE_DIR
-$env:CSGO_PROTO = $CSGO_PROTO_DIR
+$env:CSGO_PROTO = "$CSGO_PROTO_DIR\csgo"
 
 Write-Host "Using HL2SDKCS2=$env:HL2SDKCS2"
 Write-Host "Using MMSOURCE_DEV=$env:MMSOURCE_DEV"
